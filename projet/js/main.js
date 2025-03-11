c63c400f787e5bd929f440653b47602ce45c987a
Emil <somaleyoo123@gmail.com>
2025-03-11 09:45:53 +0100

    questions ajouté

--- projet/js/main.js
+++ projet/js/main.js
@@ -1,1 +1,13 @@
+ console.log("Bonjour bienvenu sur notre quizz league of legends")
+ console.log("une série de 10 question vous serons posé, a vous de bien repondre.")
+ let q1= "En quelle année la création de League of Legends a-t-elle débuté ?"
+ let q2="Combien de temps a duré la partie la plus longue de League of Legends ?"
+ let q3="Qui était le joueur le plus utile du championnat du monde 2015 ?"
+ let q4="Qui détient le record de sbires tués en une partie pro ?"
+ let q5="Combien de régions comporte Runeterra ?"
+ let q6="Quel est le vrai nom de Brand ?"
+ let q7="Et celui de Miss Fortune ?"
+ let q8="Combien de personnages utilisent des armes à feu ?"
+ let q9="Quelle est la ville la plus proche du Mont Targon ?"
+ let q10="Combien de personnages vivent au Mont Targon ?"
  
