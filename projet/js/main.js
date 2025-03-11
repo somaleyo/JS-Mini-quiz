25fd621296da76bdf227e76776c9e4bf0980559f
Emil <somaleyoo123@gmail.com>
2025-03-11 10:35:26 +0100

    score ajouté

--- projet/js/main.js
+++ projet/js/main.js
@@ -21,19 +21,22 @@ let r7="Sarah"
 let r8="17"
 let r9="Nerimazeth"
 let r10="7"
-
+let resultat = 0
 
 console.log(q1)
 let MaRep = prompt(q1)
 if (MaRep == r1){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
-    console.log("Mauvaise réponse, la bonne réponse était: "+r2)
+    console.log("Mauvaise réponse, la bonne réponse était: "+r1)
+}
     console.log(q2)
 let MaRep2 = prompt(q2)
 if (MaRep2 == r2){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r2)
@@ -42,6 +45,7 @@ console.log(q3)
 let MaRep3 = prompt(q3)
 if (MaRep3 == r3){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r3)
@@ -50,6 +54,7 @@ console.log(q4)
 let MaRep4 = prompt(q4)
 if (MaRep4 == r4){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r4)
@@ -58,14 +63,16 @@ console.log(q5)
 let MaRep5 = prompt(q5)
 if (MaRep5 == r5){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r5)
 }
 console.log(q6)
-let MaRep6 = prompt(q1)
+let MaRep6 = prompt(q6)
 if (MaRep6 == r6){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r6)
@@ -74,6 +81,7 @@ console.log(q7)
 let MaRep7 = prompt(q7)
 if (MaRep7 == r7){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r7)
@@ -82,6 +90,7 @@ console.log(q8)
 let MaRep8 = prompt(q8)
 if (MaRep8 == r8){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r8)
@@ -90,17 +99,21 @@ console.log(q9)
 let MaRep9 = prompt(q9)
 if (MaRep9 == r9){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r9)
 }
 
-}
+
 console.log(q10)
 let MaRep10 = prompt(q10)
 if (MaRep10 == r10){
     console.log("Bonne réponse !")
+    resultat++;
 }
 else{
     console.log("Mauvaise réponse, la bonne réponse était: "+r10)
 }
+
+console.log("Voici votre score: "+ resultat)
